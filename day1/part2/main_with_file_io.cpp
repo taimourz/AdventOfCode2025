a96a5178935921634f5e52d66a1e5476cb20e969
taimourz <taimour.rkt@gmail.com>
2025-12-01 17:06:36 +0500

    day1 done

---
+++ day1/part2/main_with_file_io.cpp
@@ -0,0 +1,62 @@
+/*  *
+    *
+    * Use this file when we want to do file io
+    * use fin instead  of cin
+    * use fout instead of cout
+    *
+    *
+*/
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+ifstream fin("input.txt");
+ofstream fout("output.txt");
+
+int main()
+{
+    vector<pair<string, int>> combinations;
+    string temp;
+    while(fin>>temp){
+        string left = temp.substr(0,1);
+        int right = stoi(temp.substr(1));
+        combinations.push_back(make_pair(left, right));
+    }
+
+    int count = 0;
+    int current_pos = 50;
+    for(int i = 0; i < combinations.size(); i++){
+        string dir = combinations[i].first;
+        int steps = combinations[i].second;
+
+
+        int step_dir = (dir == "R") ? 1 : -1;
+
+        for(int i = 0; i < steps; i++) {
+            current_pos = (current_pos + step_dir + 100) % 100;
+            if(current_pos == 0) count++;
+        }
+        
+    }
+
+    fout<<count<<endl;
+
+    return 0;
+
+
+}
+
+
+
+/*
+
+digits 0 to 99
+left and right indicate direction
+number indicates the number of times it should be rotated
+loops at 99 and 0
+
+
+
+
+*/
